f595b28c1607affef96f6c6ac4a90fa1c2c4244b
Uğur Kızılöz <94916789+String42nd@users.noreply.github.com>
2024-12-12 19:56:54 +0300

    traj_generator.m

--- QuadrotorSim_3DController_TrajectGen/traj_generator.m
+++ QuadrotorSim_3DController_TrajectGen/traj_generator.m
@@ -1,38 +1,4 @@
 function [ desired_state ] = traj_generator(t, state, waypoints)
-% TRAJ_GENERATOR: Generate the trajectory passing through all
-% positions listed in the waypoints list
-%
-% NOTE: This function would be called with variable number of input arguments.
-% During initialization, it will be called with arguments
-% trajectory_generator([], [], waypoints) and later, while testing, it will be
-% called with only t and state as arguments, so your code should be able to
-% handle that. This can be done by checking the number of arguments to the
-% function using the "nargin" variable, check the MATLAB documentation for more
-% information.
-%
-% t,state: time and current state (same variable as "state" in controller)
-% that you may use for computing desired_state
-%
-% waypoints: The 3xP matrix listing all the points you much visited in order
-% along the generated trajectory
-%
-% desired_state: Contains all the information that is passed to the
-% controller for generating inputs for the quadrotor
-%
-% It is suggested to use "persistent" variables to store the waypoints during
-% the initialization call of trajectory_generator.
-
-
-%% Example code:
-% Note that this is an example of naive trajectory generator that simply moves
-% the quadrotor along a stright line between each pair of consecutive waypoints
-% using a constant velocity of 0.5 m/s. Note that this is only a sample, and you
-% should write your own trajectory generator for the submission.
-
-
-
-
-%% Fill in your code here
 
 persistent coef_x coef_y coef_z waypoints0 traj_time d0
 if nargin > 2
